ca7b1478674500ff7535bcaf77c180df4424bc75
Fernanda B <fernandab@grupomundial.cr>
2022-06-03 14:32:35 -0600

    Tarea4

---
+++ Tarea4.js
@@ -0,0 +1,44 @@
+//String .concat()
+//Une dos o mas string 
+
+// let nombre = "Fernanda";
+// let apellido1 = "Barquero";
+// let apellido2 = "Vargas";
+
+// let resultado = nombre.concat(" ",apellido1," ",apellido2);
+
+// console.log(resultado);
+
+//-----------------------------------------------------------------------
+
+//String .at()
+//Devuelve el caracter en una posicion especifica del string
+//No solo debe ser entero
+
+// let nombre = "Fernanda Barquero";
+// let letra = nombre.at(-5);
+
+// console.log(letra);
+
+//-------------------------------------------------------------------------
+
+//String .charAt()
+//Devuelve el caracter en una posicion especifica del string
+//Siempre es entero
+
+// let nombre = "Fernanda Barquero";
+// let letra = nombre.charAt(5);
+
+// console.log(letra);
+
+//--------------------------------------------------------------------------
+
+//String .charCodeAt()
+//Devuelve el valor unicode del caracter
+
+// let nombre = "Fernanda Barquero";
+// let codigo  = nombre.charCodeAt(4);
+
+// console.log(codigo);
+
+
